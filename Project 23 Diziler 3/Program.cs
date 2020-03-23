021a1dfc4da2f21d88382afcb62b0bec8a849086
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-23 04:14:12 +0300

    Initial commit

---
+++ Project 23 Diziler 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_23_Diziler_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dışarıdan girilen bir ifadeyi bir char dizide tutunuz. Dizinin boyutunu gelen ifadenin uzunluğuna göre yeniden boyutlandırıp tüm elemanlarını bir ListBox’a yazdırınız.
+
+            char[] Karakter = new char[1];
+            string Kelime;
+            int x;
+            Console.WriteLine("Kelimeyi Giriniz:");
+            Kelime = Console.ReadLine();
+
+            Array.Resize(ref Karakter, Kelime.Length);
+
+            for (int i = 0; i < Karakter.Length; i++)
+            {
+                Karakter[i] = Kelime[i];
+            }
+
+            for (int i = 0; i < Karakter.Length; i++)
+            {
+                Console.Write(Karakter[i]);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Kaçıncı Elemanı Görmek İstiyorsunuz");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Karakter[x]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharp_Console_40_Diziler_3
+ 
